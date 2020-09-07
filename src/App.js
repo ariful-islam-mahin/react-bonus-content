0c36add4f7f2e85e15bdd0a9b6c980dfb48dbf15
Mahin <mahinyayaa@gmail.com>
2020-09-07 21:15:36 +0600

    react short circuit and like button discuss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 function App() {
+  const [likeColor, setLikeColor] = useState('');
+  const [loveColor, setLoveColor] = useState('');
+
+  const [RandomUser, setRandomUsers] = useState({});
+  useEffect(() => {
+    fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(data => setRandomUsers(data.results[0]))
+  }, [])
+
+  // console.log(RandomUser.name && RandomUser.name);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <ThumbUpIcon onClick={() => setLikeColor(likeColor ? '' : 'primary')} color={likeColor} style={{ fontSize: 80 }}></ThumbUpIcon>
+      <FavoriteIcon onClick={() => setLoveColor(loveColor ? '' : 'secondary')} color={loveColor} style={{ fontSize: 80}}></FavoriteIcon>
+
+      <h2>Random User Name: {RandomUser.name && RandomUser.name.first} {RandomUser.name && RandomUser.name.last}</h2>
+
     </div>
   );
 }
